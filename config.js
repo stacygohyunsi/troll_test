81ebf94a07cb628781cf29bd5baa026631144df0
stacygohyunsi <stacy.gohys@gmail.com>
2017-07-11 15:23:32 +0800

    Added storeMessage command

--- config.js
+++
@@ -1,5 +0,0 @@
-const Config = {
-	telegram: '345763778:AAFbelBVTRKvwdZ2w76toGh5vC5a_4Abaj4'
-};
-
-module.exports = Config;
--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const Config = require('./config');
 const Store = require('./components/Store');
 const Replies = require('./components/Replies');
 const Reminders = require('./components/Reminders');
+const announcements = require('./components/Announcements.js').announcements;
+const async = require('async');
 const TelegramBaseController = Telegram.TelegramBaseController;
 const RegexpCommand = Telegram.RegexpCommand;
 const telegramBot = new Telegram.Telegram(Config.telegram);
 
-const announcements = {};
 let count = 0;
 
 Reminders.handleAutomaticReminders();
@@ -20,17 +21,15 @@ function sendRemind() {
 
 class OtherwiseController extends TelegramBaseController {
     handle($) {
-        $.sendMessage('Sorry I dont understand- I ish troll bot so obviously you need to type in `troll pin`, `troll create`, `troll remind` to do something!');
+        $.sendMessage('Sorry I dont understand - I ish troll bot so obviously you need to type in `troll pin`, `troll create`, `troll remind` to do something!');
     }
 }
 
 class PingController extends TelegramBaseController {
-    pingHandler($) {
+    pingMessage($) {
         // var randInt = Math.floor(Math.random() * 
         // ((ansArr.length-1) - 0 + 1) + 0);
         let randInt = count += 1;
-        console.log('a');
-        console.log(randInt);
         let reply = Replies[randInt];
         $.sendMessage(reply[0]);
 
@@ -46,7 +45,7 @@ class PingController extends TelegramBaseController {
 
     pinMessage($) {
         let newItem = {
-            message: $,
+            message: $.message.text,
             dateAdded: new Date(),
         };
         if (announcements[$.message.chat.id] == undefined) {
@@ -54,7 +53,6 @@ class PingController extends TelegramBaseController {
         }
         announcements[$.message.chat.id].push(newItem);
         $.sendMessage('Your message have been added!');
-        Store.storeGoogleSheets();
     }
 
     remindMessage($) {
@@ -62,8 +60,8 @@ class PingController extends TelegramBaseController {
         if (chatGrpReminders != undefined && chatGrpReminders.length > 0) {
             let result = 'Here are the reminders\n';
             result = result + '=======================\n';
-            for (let i =0; i < chatGrpReminders.length; i++) {
-                let rawStr = chatGrpReminders[i].message.message.text;
+            for (let i = 0; i < chatGrpReminders.length; i++) {
+                let rawStr = chatGrpReminders[i].message;
                 let stringToAdd = rawStr.substring('troll pin '.length);
                 result += (i+1).toString() + '-' + stringToAdd + '\n';
             }
@@ -75,18 +73,50 @@ class PingController extends TelegramBaseController {
         }
     }
 
+    storeMessage($) {
+        let chatGrpReminders = announcements[$.message.chat.id];
+        if (chatGrpReminders != undefined && chatGrpReminders.length > 0) {
+            async.series([
+                Store.setAuth,
+                Store.getInfoAndWorksheets,
+                (next) => {
+                    Store.updateSheets(chatGrpReminders, next)
+                }
+            ], function(err){
+                if( err ) {
+                    console.log('Error: ' + err);
+                }
+                $.sendMessage('Reminders are stored in Google Docs.');
+            })
+        }
+    }
+
     createMessage($) {
-        let rawStr = $.message.text;
-        let stringToAdd = rawStr.substring('troll create '.length);
-        Replies.push([stringToAdd]);
+        const form = {
+            message: {
+                q: 'Send me a message to be added to your list of motivational messages.',
+                error: 'Sorry, wrong input. Please type in a message to be added to your list',
+                validator: (message, callback) => {
+                    if(message.text) {
+                        callback(true, message.text)
+                        return
+                    }
+                    callback(false)
+                }
+            }
+        }
+        $.runForm(form, (result) => {
+            Replies.push([result.message]);
+        })
     }
 
     get routes() {
         return {
-            'pingCommand': 'pingHandler',
+            'pingCommand': 'pingMessage',
             'pinCommand': 'pinMessage',
             'remindCommand': 'remindMessage',
-            'newMessageCommand': 'createMessage'
+            'newMessageCommand': 'createMessage',
+            'newStoreCommand': 'storeMessage'
         };
     }
 }
@@ -101,7 +131,11 @@ telegramBot.router
         new PingController()
     )
     .when(
-        new RegexpCommand(/^troll create /i, 'newMessageCommand'),
+        new RegexpCommand(/^troll create/i, 'newMessageCommand'),
+        new PingController()
+    )
+    .when(
+        new RegexpCommand(/^troll store/i, 'newStoreCommand'),
         new PingController()
     )
     .when(
