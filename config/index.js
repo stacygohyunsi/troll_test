d026254564797fe12c35caf472cf667d0f479e7b
stacygohyunsi <stacy.gohys@gmail.com>
2017-07-12 23:35:29 +0800

    Hide confidential keys

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const Config = {
-	telegram: '345763778:AAFbelBVTRKvwdZ2w76toGh5vC5a_4Abaj4',
-	spreadsheetKey: '1jHqgE852pXI6BQEgr2EiYBiwtBC4_22SI6kkg-MQylE'
+	telegram: 'Your key here', //345763778:AAFbelBVTRKvwdZ2w76toGh5vC5a_4Abaj4
+	spreadsheetKey: 'Your key here' //1jHqgE852pXI6BQEgr2EiYBiwtBC4_22SI6kkg-MQylE
 };
 
 module.exports = Config;
